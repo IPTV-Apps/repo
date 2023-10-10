0578b00b5df189121a6c792622d02f496d5aca9f
Andre Boutot <75226058+StreamMaine@users.noreply.github.com>
2023-10-10 05:20:51 -0400

    Delete rate.php

--- cp/3_watchit/cpsmart4/api/whmcssmarters/rate.php
+++
@@ -1,6 +0,0 @@
-<?php
-$sqlite3 = new SQLite3('../.cockpit-0001.db');
-$sql = "SELECT * FROM rate_whmcssmarters;";
-$rateus_data = $sqlite3->query($sql);
-
-header("Location: " . $rateus_data->fetchArray()['rateus_url']);
