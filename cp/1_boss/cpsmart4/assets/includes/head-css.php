7ac51487330b8fac7e789ba0d2f5ed6569923bfe
Andre Boutot <75226058+StreamMaine@users.noreply.github.com>
2023-10-10 05:01:56 -0400

    Create head-css.php

---
+++ cp/1_boss/cpsmart4/assets/includes/head-css.php
@@ -0,0 +1,10 @@
+<script src="./assets/libs/jquery/jquery.min.js"></script>
+<script src="./assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<link href="./assets/css/bootstrap-dark.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<link href="./assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+<link href="./assets/css/app-dark.min.css" id="app-style" rel="stylesheet" type="text/css" />
+<link href="./assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<link href="./assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<link href="./assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<link href="./assets/libs/twitter-bootstrap-wizard/prettify.css" rel="stylesheet"  type="text/css" />
